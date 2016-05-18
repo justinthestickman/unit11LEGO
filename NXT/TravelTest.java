aa759e0ca422839c8bd47122bd80a2da39755e30
Justin Huang <JCHuang@student.naperville203.org>
2016-05-18 15:09:57 -0500

    Finished exiting box, started detecting black line 5/18/16

--- NXT/TravelTest.java
+++ NXT/TravelTest.java
@@ -2,6 +2,7 @@ import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
+import lejos.nxt.LightSensor;
 import lejos.robotics.navigation.DifferentialPilot;
 
 /**
@@ -11,8 +12,9 @@ public class TravelTest
 {
     DifferentialPilot pilot;
     TouchSensor bump = new TouchSensor(SensorPort.S4);
+    LightSensor light = new LightSensor(SensorPort.S1);
 
-    public void go()
+    public void exitBox()
     {
         pilot.travel(10, true);
         while (pilot.isMoving())
@@ -21,18 +23,29 @@ public class TravelTest
             {
                 pilot.stop();
                 pilot.travel(-5, false);
-                pilot.rotate(80);
+                pilot.rotate(110);
                 pilot.travel(10, true);
             }
         }
         System.out.println(" "+pilot.getMovement().getDistanceTraveled());
         Button.waitForAnyPress();
     }
+    
+    public void goHam()
+    {
+        while(pilot.isMoving())
+        {
+            if (light.getLightValue()<5)
+            {
+                
+            }
+        }
+    }
 
     public static void main(String[] args)
     {
         TravelTest traveler = new TravelTest();
         traveler.pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.C);
-        traveler.go();
+        traveler.exitBox();
     }
 }
