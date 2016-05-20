570571ffe1dc835a933b77dba71648ab31c2bd9c
Justin Huang <JCHuang@student.naperville203.org>
2016-05-20 15:10:46 -0500

    Finished...probably...hopefully 5/20/16

--- NXT/Robot.java
+++ NXT/Robot.java
@@ -1,5 +1,7 @@
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.MotorPort;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
 import lejos.nxt.LightSensor;
@@ -9,15 +11,18 @@ import lejos.nxt.LCD;
 /**
  * 
  */
-public class TravelTest
+public class Robot
 {
     DifferentialPilot pilot;
+    NXTRegulatedMotor right = new NXTRegulatedMotor(MotorPort.A);
+    NXTRegulatedMotor left = new NXTRegulatedMotor(MotorPort.C);
     TouchSensor bump1 = new TouchSensor(SensorPort.S1);
     TouchSensor bump2 = new TouchSensor(SensorPort.S4);
     LightSensor light = new LightSensor(SensorPort.S3);
     
     public void goHam()
     {
+        //Exit box
         pilot.travel(12, true);
         while (pilot.isMoving())
         {
@@ -29,25 +34,41 @@ public class TravelTest
                 pilot.travel(12, true);
             }
         }
-        //System.out.println(" "+pilot.getMovement().getDistanceTraveled());
         
+        //Travel to black edge of circle
         pilot.travel(50, true);
         while(pilot.isMoving())
         {
-            if (light.getLightValue() < 20)
+            if (light.getLightValue() < 30)
             {
                 pilot.stop();
                 System.out.println(light.getLightValue());
                 LCD.drawInt(light.getLightValue(),0,0,0);
             }
         }
+        
+        //Travel around circle
+        pilot.travel(4.5, false);
+        pilot.rotate(-90);
+        pilot.arc(53.5, -360);
+    }
+    
+    public void lightSensorTest()
+    {
+        System.out.println("Place on white");
+        Button.waitForAnyPress();
+        System.out.println("White value: " + light.getLightValue());
+        System.out.println("Place on black");
+        Button.waitForAnyPress();
+        System.out.println("Black value: " + light.getLightValue());
         Button.waitForAnyPress();
     }
 
     public static void main(String[] args)
     {
-        TravelTest traveler = new TravelTest();
-        traveler.pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.C);
-        traveler.goHam();
+        Robot joe = new Robot();
+        joe.pilot = new DifferentialPilot(5.6f, 16f, Motor.A, Motor.C);
+        //joe.lightSensorTest();
+        joe.goHam();
     }
 }
